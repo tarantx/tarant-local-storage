ad4d94105974b2cc1d30dfca0ce7cff207cbf368
kanekotic <me@kanekotic.com>
2022-10-18 09:41:32 +0200

    Alvaro | updated dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,9 @@ import { uglify } from "rollup-plugin-uglify"
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
 
 export default {
     output: {

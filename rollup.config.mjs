821688e238123c48c985e07b3f384d4e24f70d65
kanekotic <me@kanekotic.com>
2022-10-18 09:52:05 +0200

    Alvaro | update build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,9 @@
-import { uglify } from "rollup-plugin-uglify"
+import { terser } from "rollup-plugin-terser"
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
+import json from "rollup-plugin-json"
+import builtins from "rollup-plugin-node-builtins"
 import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
@@ -14,16 +16,15 @@ export default {
     },
     input: "lib/index.ts",
     plugins: [
-        typescript({
-            tsconfig: false,
-            target: "es5",
-            declaration: true,
-            strict: true,
-            lib: ["es6"],
-
-        }),
-        nodeResolve(), 
+        nodeResolve({ jsnext: true, preferBuiltins: true, browser: true }), 
         commonjs(), 
-        uglify()
+        builtins(),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            objectHashIgnoreUnknownHack: true,
+            tsconfigOverride: { compilerOptions: { module: "ESNext" } }
+        }),
+        terser(),
+        json()
     ]
 }
